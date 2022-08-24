93e1b5e706a4468b4587f299c812bbee011ca8cb
Mishra-Atharva <111166413+Mishra-Atharva@users.noreply.github.com>
2022-08-24 18:30:10 +0800

    updated version

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // key = 5dtZbbQQqOMHmCEZvv6uOacINBFYSmyf
+console.log("testing...");
 var myHeaders = new Headers();
     myHeaders.append("apikey", "5dtZbbQQqOMHmCEZvv6uOacINBFYSmyf");
     var requestOptions = {
@@ -6,18 +7,27 @@ var myHeaders = new Headers();
         redirect: 'follow',
         headers: myHeaders
       };
+    console.log("https://api.apilayer.com/currency_data/list"+Object.keys(requestOptions));
 
 function getCountries() {
 
     fetch("https://api.apilayer.com/currency_data/list", requestOptions)
       .then((a) => a.json())
       .then((response) => {
-        console.log(response.currencies);
-        var option = document.createElement("select", "selection");
+        // console.log(Object.keys(response.currencies).length);
+        //console.log(Object.keys(response.currencies)[0]);
+        for (i = 0; i < Object.keys(response.currencies).length; i++) {
+          var list = document.getElementById("list");
+          var option = document.createElement("option");
+          option.text = Object.keys(response.currencies)[i];
+          option.value = Object.keys(response.currencies)[i];
+          list.append(option);
+        }
+        
       })
-}
+  }
 
-// fetch("https://api.apilayer.com/currency_data/list", requestOptions)
+// fetch("https://api.currencylayer.com/convert&from=GBP&to=USD&amount=10")
 //   .then(response => response.text())
 //   .then(result => console.log(result))
 //   .catch(error => console.log('error', error));
